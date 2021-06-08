aec9031412d33d43780a37544450b5d7ef9349d6
CosName <502526469@qq.com>
2021-06-08 15:46:16 +0800

    Merge 11

--- packages/check-pre-push/bin/commit.js
+++ packages/check-pre-push/bin/commit.js
@@ -3,33 +3,28 @@
 const { print_info } = require("../lib/print");
 const { getConf } = require('../lib/getConfig');
 const getCommitMsg = require("../lib/getCommitMsg");
-var MERGE_COMMIT_PATTERN = /^Merge/;
+
+const { commitTag } = getConf('check-pre-push');
+const COMMIT_PATTERN = new RegExp(`^(${commitTag.join('|')})(\\([^\\)]*\\))?:`);
+const MERGE_COMMIT_PATTERN = /^Merge/;
 
 
 async function checkCommitMsg() {
     return new Promise(function(resolve, reject) {
         print_info('check', 'commit_message');
         let { message, sourceFile } = getCommitMsg(); // commit message
-        const { commitTag } = getConf('check-pre-push');
-        const commit_tags_pattern = commitTag.join('|');
-        const commit_pattern_str = `^(${commit_tags_pattern})(\\([^\\)]*\\))?:`;
-        const commit_pattern = new RegExp(commit_pattern_str);
-        const result = commit_pattern.test(message);
+        const result = COMMIT_PATTERN.test(message);
         if (result) {
             resolve();
         } else {
-            console.log(MERGE_COMMIT_PATTERN);
-            console.log(message);
-            console.log(MERGE_COMMIT_PATTERN.test(message));
             if (MERGE_COMMIT_PATTERN.test(message)) {
                 console.log('Merge commit detected.');
                 resolve();
-            } else {
-                const err = `commit message不符合规范  \n` +
-                    `commit message: "${message}" \n` +
-                    `Pattern: ${commit_pattern}`;
-                reject(err);
             }
+            const err = `commit message不符合规范  \n` +
+                `commit message: "${message}" \n` +
+                `Pattern: ${COMMIT_PATTERN}`;
+            reject(err);
         }
     });
 }
